0f8ffb67e35bd0bffc0765b391ab1baf671bdad1
jomrembau <jomir.bautista@gmail.com>
2026-03-18 18:03:41 +0100

    Refactor tipTheWaiter to return tip amount

--- Tip.java
+++ Tip.java
@@ -1,8 +1,8 @@
 public class Tip {
     public static void main(String[] args) {
-        tipTheWaiter(65.00, .15);
+        System.out.println(tipTheWaiter(65.00, .15));
     }
-    public static void tipTheWaiter(double bill, double tip){
-        System.out.println("Your service was wonderful! Please, accept this tip: $" + (bill * tip));
+    public static double tipTheWaiter(double bill, double tip){
+        return bill*tip;
     }
 }
